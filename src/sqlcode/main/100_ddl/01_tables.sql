f75a2fff22679eabfd8f0b59ba3ef7fc4bc94f9f
Adeel Malik <amalik@liquibase.com>
2022-06-22 13:35:34 -0500

    first commit

---
+++ src/sqlcode/main/100_ddl/01_tables.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+
+--changeset SteveZ:765
+CREATE TABLE person (foo VARCHAR, PRIMARY KEY (foo))
+--rollback DROP TABLE immatable;
+--changeset SteveZ:766
+CREATE TABLE company (foo2 VARCHAR, PRIMARY KEY (foo2))
+--rollback DROP TABLE immatable2;
+
+--changeset AdeelM:898
+CREATE TABLE orders (foo2 VARCHAR, PRIMARY KEY (foo2))
+--rollback DROP TABLE orders;
